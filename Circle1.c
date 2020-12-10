2d468187e2d47375495d5cc21b2629cdd8837f57
mayurkhalkar <71220402+mayurkhalkar@users.noreply.github.com>
2020-12-09 16:33:12 -1100

    Add files via upload

---
+++ Circle1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float radius, diameter, circumference, area;
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+    diameter = 2 * radius;
+    circumference = 2 * 3.14 * radius;
+    area = 3.14 * (radius * radius);
+printf("Diameter of circle = %f\n", diameter);
+    printf("Circumference of circle = %f \n", circumference);
+    printf("Area of circle = %f ", area);
+    float sp =(1.33) * 3.14 * (radius*radius*radius);
+    printf("Valume Of sphere = %f ",sp);
+}
+
